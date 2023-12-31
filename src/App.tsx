75276b106ad0629d7af8a94567408c1b96ec0c33
Nicholas Rubright <koalrube@gmail.com>
2023-12-30 21:00:12 -0500

    updated favicon and styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Summary from "./components/Summary";
 function App() {
   return (
     <div className="container col-xxl-8 px-4 py-5">
-      <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div className="row justify-content-md-center justify-content-sm-center flex-lg-row-reverse align-items-center g-5 py-5">
         <div className="col-lg-6">
           <Summary />
           <SocialMedia />
--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -3,12 +3,16 @@ export default function SocialMedia() {
     <div className="d-grid gap-2 d-md-flex justify-content-md-start">
       <a
         href="https://www.linkedin.com/in/nicholas-rubright-114087178/"
+        target="_blank"
+        rel="noopener noreferrer"
         className="btn btn-lg btn-primary"
       >
         <i className="bi bi-linkedin" /> Linkedin
       </a>
       <a
         href="https://github.com/nicholasrubright"
+        target="_blank"
+        rel="noopener noreferrer"
         className="btn btn-lg btn-primary"
       >
         <i className="bi bi-github" /> Github
